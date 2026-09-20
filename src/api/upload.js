cbb5d07668e8b480af01a3ec6456fa36fe74fcef
sh2624 <sh2624@daum.net>
2026-09-20 19:24:19 +0900

    Fix: 사진 업로드 시 헤더 타입 변경

--- src/api/upload.js
+++ src/api/upload.js
@@ -6,7 +6,11 @@ export const analyzeImage = async (imageFile) => {
   const formData = new FormData();
   formData.append('file', imageFile);
 
-  const response = await api.post('/upload', formData);
+  const response = await api.post('/upload', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
 
   return response.data;
 };
