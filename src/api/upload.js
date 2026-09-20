4de73cbd41c3e87db8c26ecfc24a2f5b5fff9522
sh2624 <sh2624@daum.net>
2026-09-20 16:26:02 +0900

    Fix: 사진 업로드 API 수정

--- src/api/upload.js
+++ src/api/upload.js
@@ -4,13 +4,9 @@ import api from './axios';
 // 사진 업로드
 export const analyzeImage = async (imageFile) => {
   const formData = new FormData();
-  formData.append('file', imageFile);
+  formData.append('image', imageFile);
 
-  const response = await api.post('/upload', formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
+  const response = await api.post('/upload', formData);
 
   return response.data;
 };
